56c7844fbd5fadd04e6cc078c0d16b5c1d602c06
jmartino17 <jordan_martino@oru.edu>
2025-09-17 09:59:04 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,12 @@
+// Define Variables
+let myName = "Jordan Martino";
+let title = "Student";
+let education = "Oral Roberts University";
+let experience = "Five Guys, West End Creamery Concessions Stand, Mitchell Differential Inc.";
+let skills = ["Customer service", "Teamwork", "Attention to detail"];
+
+// Connect Variables to the HTML
+document.getElementById("my-name").textContent = myName;
+document.getElementById("title").textContent = title;
+document.getElementById("education").textContent = education;
+document.getElementById("experience").textContent = experience;
