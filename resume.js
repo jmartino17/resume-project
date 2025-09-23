3400e164cf7d0d1aeef763c67959b7ee97404ecc
jmartino17 <jordan_martino@oru.edu>
2025-09-23 15:03:07 -0500

    resume.js

--- resume.js
+++ resume.js
@@ -1,9 +1,24 @@
 // Define Variables
 let myName = "Jordan Martino";
+
 let title = "Student";
+
 let education = "Oral Roberts University";
+
 let experience = "Five Guys, West End Creamery Concessions Stand, Mitchell Differential Inc.";
-let skills = ["Customer service", "Teamwork", "Attention to detail"];
+
+let skills = ["HTML", "CSS", "JavaScript"];
+let skillsList = document.createElement("ul");
+for (let i = 0; i < skills.length; i++) {
+ let li = document.createElement("li");
+ li.textContent = skills[i];
+ skillsList.appendChild(li);
+}
+document.body.appendChild(skillsList);
+
+function showFunFact() {
+ alert("Fun Fact: I love ice skating!");
+}
 
 // Connect Variables to the HTML
 document.getElementById("my-name").textContent = myName;
